c0cdfb4da173b41b868f1c629ce232c35a6fbdbb
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-11-17 13:26:27 +0100

    set up react-router-dom

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import Register from './Components/Authentication/Register/Register';
+import Login from './Components/Authentication/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/register' Component={Register} />
+          <Route path='/login' Component={Login} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
