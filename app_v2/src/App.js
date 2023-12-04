f8d0ddb12ed5fb59af5b3374fcd4b98eb69220dd
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-12-04 18:50:33 +0100

    create a Sidebar component and setup react router dom

--- app_v2/src/App.js
+++ app_v2/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+
+import { Routes, Route, useLocation } from 'react-router-dom';
+
+// Components
+import Home from './Pages/Home/Home';
+import Sidebar from './Layout/Sidebar/Sidebar';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Projects from './Pages/Dashboard/Projects';
+import Tickets from './Pages/Dashboard/Tickets';
+import Settings from './Pages/Dashboard/Settings';
+
+// Styles
 import './App.scss';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className='container'>
+      {/* Sidebar will be rendered for all routes except '/' */}
+      {location.pathname !== '/' && <Sidebar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/projects" element={<Projects />} />
+        <Route path="/dashboard/tickets" element={<Tickets />} />
+        <Route path="/dashboard/settings" element={<Settings />} />
+      </Routes>
+    </div>
   );
 }
 
--- app_v2/src/Layout/Sidebar/Sidebar.js
+++ app_v2/src/Layout/Sidebar/Sidebar.js
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+// Icons
+import { FaFolder, FaTicketAlt } from "react-icons/fa";
+import { MdDashboard } from "react-icons/md";
+import { MdSettings, MdLogout } from "react-icons/md";
+// Images
+import Logo from '../../assets/Logo.png'
+import Avatar from '../../assets/avatar.jpg'
+// Styles
+import './Sidebar.scss'
 
 const Sidebar = () => {
+  const links = [
+    {
+      id: 0,
+      path: '/dashboard',
+      name: 'Dashboard',
+      icon: <MdDashboard className='icon' />
+    },
+    {
+      id: 1,
+      path: '/dashboard/projects',
+      name: 'Projects',
+      icon: <FaFolder className='icon folder' />
+    },
+    {
+      id: 2,
+      path: '/dashboard/tickets',
+      name: 'Tickets',
+      icon: <FaTicketAlt className='icon' />
+    },
+    {
+      id: 3,
+      path: '/dashboard/settings',
+      name: 'Settings',
+      icon: <MdSettings className='icon' />
+    }
+  ];
+
+  const [activeLink, setActiveLink] = useState(
+    localStorage.getItem('linkIndex') || 0
+  );
+
+  const handleActiveLinkChange = (index) => {
+    setActiveLink(index);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('linkIndex', activeLink);
+  }, [activeLink]);
+
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      <div className='logo'>
+        <Link to={'/'}>
+          <img src={Logo} alt='Logo' />
+        </Link>
+      </div>
+
+      <ul className='sidebar__links'>
+        {links.map((link, index) => <li key={index} onClick={() => handleActiveLinkChange(index)}>
+          <Link
+            className={index == activeLink ? 'sidebar__links__ele active' : 'sidebar__links__ele'}
+            to={link.path}
+          >
+            {link.icon} {link.name}
+          </Link>
+        </li>)}
+      </ul>
+
+      <div className='profile'>
+        <div className='profile__info'>
+          <div className='profile__img'>
+            <img src={Avatar} alt='avatar' />
+          </div>
+          <div className='profile__info__nr'>
+            <h4>Youssef Nasrallah</h4>
+            <p>Full-stack Web developer</p>
+          </div>
+        </div>
+        <div className='logout'>
+          <MdLogout className='icon' />
+        </div>
+      </div>
+    </div>
   )
 }
 
